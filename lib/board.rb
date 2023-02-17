6a62880894c9bfc6e6d922ca0f67c895650b1c7a
Caroline Peri <caroline.anne.peri@gmail.com>
2023-02-16 22:11:57 -0500

    feat: add make_grid helper method

--- lib/board.rb
+++ lib/board.rb
@@ -1,11 +1,18 @@
 class Board
   attr_reader :header,
               :rows,
-              :columns
+              :columns,
+              :grid
 
   def initialize()
     @header = ["A", "B", "C", "D", "E", "F", "G"]
     @rows = 6
     @columns = 7
+    @grid = make_grid
   end
+
+  def make_grid
+    Array.new(@rows, Array.new(@columns, "."))
+  end
+
 end
