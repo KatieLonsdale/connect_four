e1fbf59f399acba6799668c3ef20372fb8fec222
Caroline Peri <caroline.anne.peri@gmail.com>
2023-02-18 18:24:06 -0500

    test: add test for get_computer_selection method

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe Game do
   before(:each) do
     @game = Game.new
+    @valid_columns = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
   end
 
   describe '#initialize' do
@@ -24,6 +25,17 @@ describe Game do
       expect(@game.computer_player).to be_a Player
       expect(@game.computer_player.is_computer?).to be true
       expect(@game.computer_player.is_human?).to be false
+    end
+  end
+
+  describe '#get_computer_selection' do
+      before(:each) do
+        @computer_selection = @game.get_computer_selection
+      end
+    it 'returns a valid column selection' do
+      expect(@computer_selection).to be_a String
+      expect(@computer_selection.length).to eq(1)
+      expect(@valid_columns).to include(@computer_selection)
     end
   end
 end
