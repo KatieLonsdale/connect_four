1d45843d65bdb57a6f73e7cbe2fbcfc6d1e174f1
Caroline Peri <caroline.anne.peri@gmail.com>
2023-02-18 18:52:25 -0500

    feat: add get_computer_selection method and open_column helper method

--- lib/game.rb
+++ lib/game.rb
@@ -8,4 +8,23 @@ class Game
     @human_player = Player.new(:human)
     @computer_player = Player.new(:computer)
   end
+
+  def get_computer_selection
+    valid_columns = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+    selection = valid_columns.sample
+    until open_column?(selection)
+      selection = valid_columns.sample
+    end
+    selection
+  end
+
+  #get human selection
+
+  #valid_letter?(selection)
+    #has to check if the column is inside the range A-G
+
+  def open_column?(selection)
+    @board.grid[selection].include?(".")
+  end
+
 end
