02071e3e615f7c34bc9bec1c42ab663176ae55c1
Caroline Peri <caroline.anne.peri@gmail.com>
2023-02-18 17:06:31 -0500

    test: add tests for initialize method

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,5 +1,29 @@
 require 'spec_helper'
 
 describe Game do
+  before(:each) do
+    @game = Game.new
+  end
 
+  describe '#initialize' do
+    it 'exists' do
+      expect(@game).to be_a Game
+    end
+
+    it 'has a board' do
+      expect(@game.board).to be_a Board
+    end
+
+    it 'has a human player' do
+      expect(@game.human_player).to be_a Player
+      expect(@game.human_player.is_human?).to be true
+      expect(@game.human_player.is_computer?).to be false
+    end
+
+    it 'has a computer player' do
+      expect(@game.computer_player).to be_a Player
+      expect(@game.computer_player.is_computer?).to be true
+      expect(@game.computer_player.is_human?).to be false
+    end
+  end
 end
