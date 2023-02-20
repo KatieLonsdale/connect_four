4099fe5f199c901d7c8b1c767ee7adae0088b855
Caroline Peri <caroline.anne.peri@gmail.com>
2023-02-20 16:53:56 -0500

    feat: update game runner to run with new ConnectFour class

--- game_runner.rb
+++ game_runner.rb
@@ -1,9 +1,4 @@
 require './spec/spec_helper'
-# initialize
-board = Board.new
-# call start/play method
-# print board/welcome message
 
-p 'Welcome to Connect Four!'
-# p 'Enter p to play or q to quit.'
-board.print_board
+connect_four = ConnectFour.new
+connect_four.start
